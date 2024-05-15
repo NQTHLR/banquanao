5306eb6d61c6d0d4f388d55f0fa68d0b2c6d0600
NQTHLR <126398372+NQTHLR@users.noreply.github.com>
2024-05-15 21:00:00 +0700

    Update connect_20230211214344.php

--- .history/db/connect_20230211214344.php
+++ .history/db/connect_20230211214344.php
@@ -1,9 +1,9 @@
 <?php
 define("CURRENCY", "৳");
 
-$server = "localhost";
-$username = "root";
-$password = "";
+$server = "banquanaoserver.mysql.database.azure.com";
+$username = "sqladmin";
+$password = "#Nqthlr123";
 $database = "eco_admin";
 $conn = mysqli_connect($server, $username, $password, $database);
 if (!$conn) {
